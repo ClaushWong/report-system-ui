0f1390da2cd336e8594d2775695a71d5341b9c75
Claush Wong <claushwong123@gmail.com>
2023-04-16 19:49:59 +0800

    fix issue

--- app/services/ui.tsx
+++ app/services/ui.tsx
@@ -10,44 +10,47 @@ type EmptyFC = () => void;
 // error - used in every try catch clause
 // info - used for debug purpose for now
 export const notify = {
-  success: (message: string | React.ReactNode) => {
-    Notification.success({ message });
-  },
-  warning: (message: string | React.ReactNode) => {
-    Notification.warn({ message });
-  },
-  error: (error: string | Error | React.ReactNode | any) => {
-    if (error instanceof Error) {
-      Notification.error({ message: error.message ?? error });
-    } else if (typeof error === "object" && error.hasOwnProperty("message")) {
-      Notification.error({ message: error.message ?? error });
-    } else {
-      Notification.error({ message: error });
-    }
-  },
-  info: (message: string | React.ReactNode) => {
-    Notification.info({ message });
-  },
+    success: (message: string | React.ReactNode) => {
+        Notification.success({ message });
+    },
+    warning: (message: string | React.ReactNode) => {
+        Notification.warning({ message });
+    },
+    error: (error: string | Error | React.ReactNode | any) => {
+        if (error instanceof Error) {
+            Notification.error({ message: error.message ?? error });
+        } else if (
+            typeof error === "object" &&
+            error.hasOwnProperty("message")
+        ) {
+            Notification.error({ message: error.message ?? error });
+        } else {
+            Notification.error({ message: error });
+        }
+    },
+    info: (message: string | React.ReactNode) => {
+        Notification.info({ message });
+    },
 };
 
 // confirm popup
 // used before perform any operation related to send data to API
 export const confirm = (
-  message: string,
-  yes?: EmptyFC,
-  opt?: {
-    icon?: React.ReactNode;
-    okText?: string;
-    cancelText?: string;
-    no?: EmptyFC;
-  }
+    message: string,
+    yes?: EmptyFC,
+    opt?: {
+        icon?: React.ReactNode;
+        okText?: string;
+        cancelText?: string;
+        no?: EmptyFC;
+    }
 ) => {
-  Modal.confirm({
-    icon: opt?.icon ?? <ExclamationCircleOutlined />,
-    content: message,
-    onOk: yes,
-    onCancel: opt?.no,
-    okText: opt?.okText,
-    cancelText: opt?.cancelText,
-  });
+    Modal.confirm({
+        icon: opt?.icon ?? <ExclamationCircleOutlined />,
+        content: message,
+        onOk: yes,
+        onCancel: opt?.no,
+        okText: opt?.okText,
+        cancelText: opt?.cancelText,
+    });
 };
