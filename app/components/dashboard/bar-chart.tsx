6f00d4a1fc5f2e32cff34f9d5a67175d94159e96
Claush Wong <claushwong123@gmail.com>
2024-04-27 21:19:16 +0800

    update: dashboard

--- app/components/dashboard/bar-chart.tsx
+++ app/components/dashboard/bar-chart.tsx
@@ -1,56 +1,63 @@
 import React from "react";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
+import "chartjs-plugin-datalabels";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 type BarChartProps = {
-    data: any[];
-    decimal: number;
-    width?: number;
-    height?: number;
+  data: any[];
+  decimal: number;
+  width?: number;
+  height?: number;
 };
 
 export default function App(props: BarChartProps) {
-    const options = {
-        responsive: true,
-        maintainAspectRatio: true,
-        plugins: {
-            legend: {
-                display: false,
-            },
-            title: {
-                display: false,
-            },
-        },
-    };
+  const options = {
+    responsive: true,
+    maintainAspectRatio: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+  };
 
-    const labels = props?.data?.map((i) => i.label);
-    const values = props?.data?.map((i) => i.value.toFixed(props.decimal));
+  const labels = props?.data?.map((i) => i.label);
+  const values = props?.data?.map((i) => i.value.toFixed(props.decimal));
 
-    const data = {
-        labels,
-        datasets: [
-            {
-                data: values,
-                backgroundColor: "rgba(200, 99, 132, 0.5)",
-            },
-        ],
-    };
-    return <Bar options={options} data={data} height={9} width={16} />;
+  const data = {
+    labels,
+    datasets: [
+      {
+        data: values,
+        backgroundColor: "rgba(200, 99, 132, 0.5)",
+      },
+    ],
+    plugins: {
+      datalabels: {
+        display: true,
+        color: "black",
+      },
+    },
+  };
+  return <Bar options={options} data={data} height={9} width={16} />;
 }
